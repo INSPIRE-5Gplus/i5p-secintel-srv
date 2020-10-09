36f15d77633c3fef9ffdcf7c1bc6b0e8df56136f
Ricard Vilalta <rvilalta@gmail.com>
2020-10-09 13:55:51 +0200

    Runnning on proper ports and new requirement requests

--- client.sh
+++ client.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 
-curl -X GET -H "Content-Type: application/json" http://127.0.0.1:8182/monitoring_action/a
-curl -X GET -H "Content-Type: application/json" http://127.0.0.1:8182/monitoring_action
+curl -X GET -H "Content-Type: application/json" http://127.0.0.1:4455/monitoring_action/a
+curl -X GET -H "Content-Type: application/json" http://127.0.0.1:4455/monitoring_action
 
-curl -X POST -H "Content-Type: application/json" http://127.0.0.1:8182/monitoring_action/a -d'{"action_id" : "a","action_type" : "MONITOR_IDS","element_ip" : "10.55.0.14","notify_violation" : "policy_manager", "debug" : "True"}'
+curl -X POST -H "Content-Type: application/json" http://127.0.0.1:4455/monitoring_action/a -d'{"action_id" : "a","action_type" : "MONITOR_IDS","element_ip" : "10.55.0.13:8080","notify_violation" : "policy_manager", "debug" : "True"}'
 
-curl -X DELETE -H "Content-Type: application/json" http://127.0.0.1:8182/monitoring_action/a
+curl -X DELETE -H "Content-Type: application/json" http://127.0.0.1:4455/monitoring_action/a
 
 
 
 #ACTING AS IDS:
 
-curl -X POST -H "Content-Type: application/json" http://127.0.0.1:8182/telemetry/a -d'{"id":1234, "trust" : 0}'
+curl -X POST -H "Content-Type: application/json" http://127.0.0.1:4455/telemetry/a -d'{"id":1234, "trust" : 0}'
 
--- secintel.py
+++ secintel.py
@@ -18,7 +18,8 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-IP_SECINTEL="10.55.0.13:4455"
+IP_SECINTEL="10.55.0.13"
+PORT_SECINTEL="4455"
 
 @app.route('/')
 @cross_origin()
@@ -41,7 +42,7 @@ def create_monitoring_action(uuid):
     monitoring_action_db[uuid]=data
     if monitoring_action_db[uuid]['action_type'] == "MONITOR_IDS":
         #CREATE MONITORING CALLBACK_URL
-        CALLBACK_URL = "http://" + IP_SECINTEL + "/telemetry/" + uuid
+        CALLBACK_URL = "http://" + IP_SECINTEL + ":" + PORT_SECINTEL + "/telemetry/" + uuid
         json_data = { "callback_url" : CALLBACK_URL, "enable" : 1 }
         #SUBSCRIBE TO ELEMENT WITH MONITORING URL
         URL = "http://" + monitoring_action_db[uuid]["element_ip"] + "/subscribe/"
@@ -80,5 +81,5 @@ if __name__ == "__main__":
     
     #flask
     logger.info("initializing flask")
-    port = os.environ.get('PORT_HTTP', "8182")
+    port = os.environ.get('PORT_HTTP', PORT_SECINTEL)
     app.run(debug=False,host='0.0.0.0', port=port)
